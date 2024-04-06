9e9ddb4c8a7424da71b61b6f66b57cbb0ae7a4db
João Pedro Lopes <joaopalopes24@gmail.com>
2024-04-06 20:16:48 -0300

    refactor(menu): adjust icon in menu header

--- src/stores/menu.ts
+++ src/stores/menu.ts
@@ -1,19 +1,11 @@
 // ** External Imports
-import { find } from "lodash";
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
 // ** Internal Imports
 import { MenuType } from "@/config/menu";
 
-interface MenuItem {
-  id: string;
-  open: boolean;
-}
-
 interface Menu {
   visible: boolean;
-  menu: MenuItem[];
   page: string | null;
   hidden: boolean | null;
   toggleVisible: () => void;
@@ -23,30 +15,20 @@ interface Menu {
   setVisible: (visible: boolean) => void;
 }
 
-const useMenu = create<Menu>()(
-  persist(
-    (set, get) => ({
-      menu: [],
-      page: null,
-      hidden: true,
-      visible: false,
+const useMenu = create<Menu>()((set, get) => ({
+  page: null,
+  hidden: true,
+  visible: false,
 
-      setPage: (page?: string) => set({ page }),
+  setPage: (page?: string) => set({ page }),
 
-      setHidden: (hidden: boolean) => set({ hidden }),
+  setHidden: (hidden: boolean) => set({ hidden }),
 
-      setVisible: (visible: boolean) => set({ visible }),
+  setVisible: (visible: boolean) => set({ visible }),
 
-      checkLink: (item: MenuType) => get().page === item.id,
+  checkLink: (item: MenuType) => get().page === item.id,
 
-      toggleVisible: () => set((state) => ({ visible: !state.visible })),
-    }),
-    {
-      name: "menu",
-      skipHydration: true,
-      partialize: (state) => ({ menu: state.menu }),
-    },
-  ),
-);
+  toggleVisible: () => set((state) => ({ visible: !state.visible })),
+}));
 
 export default useMenu;
